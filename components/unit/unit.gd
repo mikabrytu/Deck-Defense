e46b9483ce9cbab9192e072804de439e978698bc
Mikabrytu <mikabrytu@gmail.com>
2023-11-01 13:55:26 -0300

    Add optional skip step to find closest enemy function

--- components/unit/unit.gd
+++ components/unit/unit.gd
@@ -8,6 +8,7 @@ enum UNIT_STATE { IDLE, SEEKING, ATTACKING }
 
 var _health: Health
 var _movement: Movement
+var _anim_converter: PlayerAnimConveter
 var _agent: NavigationAgent3D
 var _attack_interval: Timer
 var _target: Vector3
@@ -22,19 +23,17 @@ func _physics_process(_delta):
 	if _target == null:
 		return
 	
+	look_at(_target, Vector3.UP, true)
+	
 	if _state == UNIT_STATE.SEEKING:
 		var distance = position.distance_to(_target)
 		if distance <= distance_to_target:
-			if _state != UNIT_STATE.ATTACKING:
-				_state = UNIT_STATE.ATTACKING
-			
+			change_state(UNIT_STATE.ATTACKING)
 			return
 	
 		var current = global_transform.origin
 		var next = _agent.get_next_path_position()
 		var direction = (next - current).normalized()
-		
-		look_at(_target, Vector3.UP, true)
 		
 		_movement.move(direction)
 
@@ -45,18 +44,20 @@ func _physics_process(_delta):
 func set_unit():
 	_health = $Health as Health
 	_movement = $Movement as Movement
+	_anim_converter = $"Animation Converter" as PlayerAnimConveter
 	_agent = $Agent as NavigationAgent3D
 	_attack_interval = $"Attack Interval" as Timer
 	
 	_health.dead.connect(_on_dead)
 	
-	_state = UNIT_STATE.IDLE
+	change_state(UNIT_STATE.IDLE)
 
 
 func seek_target(target: Vector3):
 	_target = target
 	_agent.set_target_position(_target)
-	_state = UNIT_STATE.SEEKING
+	
+	change_state(UNIT_STATE.SEEKING)
 
 
 func start_attacking(callable: Callable):
@@ -77,12 +78,15 @@ func set_targeted(active: bool = true):
 	_is_targeted = active
 
 
-func find_closest_enemy(group: String) -> Unit:
+func find_closest_enemy(group: String, ignore_targeted: bool = true) -> Unit:
 	var enemies = get_tree().get_nodes_in_group(group)
 	var enemy = null
 	var shortest = 100
 	
 	for e in enemies:
+		if not ignore_targeted and e.is_targeted():
+			continue
+		
 		var distance = position.distance_to(e.position)
 		
 		if shortest > distance:
@@ -90,6 +94,22 @@ func find_closest_enemy(group: String) -> Unit:
 			enemy = e
 	
 	return enemy
+
+
+func change_state(state: UNIT_STATE):
+	_state = state
+	
+	if _anim_converter == null:
+		return
+	
+	if state == UNIT_STATE.IDLE:
+		_anim_converter.idle()
+	
+	if state == UNIT_STATE.SEEKING:
+		_anim_converter.run()
+	
+	if state == UNIT_STATE.ATTACKING:
+		_anim_converter.attack()
 
 
 # Listeners
